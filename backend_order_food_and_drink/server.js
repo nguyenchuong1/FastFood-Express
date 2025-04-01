5568ed0c814bdbdebf20a5a0699d6d71dcddd70d
Lý Quốc Thành <lyquocthanh.work@gmail.com>
2025-04-01 21:55:05 +0700

    Add files via upload

---
+++ backend_order_food_and_drink/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use('/static', express.static('static'));
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to application." });
+});
+
+require("./app/routes/customer.routes")(app);
+require("./app/routes/login.routes")(app);
+require("./app/routes/category.routes")(app);
+require("./app/routes/product.routes")(app);
+require("./app/routes/cart.routes")(app);
+require("./app/routes/order.routes")(app);
+require("./app/routes/paymnet.routes")(app);
+require("./app/routes/revenue.routes")(app);
+
+const socket = require("./app/socket");
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
