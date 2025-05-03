15326eacd6fcd002e945bb63e1583459df3352c0
Minh Khoa <hiphopck1@gmail.com>
2025-05-03 23:05:03 +0700

    First push project to GitHub

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Đặt hàng ngay</h2>
+            <p>Chỉ một cú chạm, món ngon đã sẵn sàng – Nhanh gọn cho cuộc sống bận rộn!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
